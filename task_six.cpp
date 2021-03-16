3df815ff1b8495272294a57cfb0a8a5dac0ea43b
iZarub <71636846+iZarub@users.noreply.github.com>
2021-03-16 19:13:03 +0300

    Create task_six.cpp

---
+++ task_six.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<vector>
+
+
+using std::vector;
+using std::cout;
+using std::cin;
+
+
+
+void show(vector<int>v, int S){
+    for (int i = 0; i < S; i++){
+        cout << v[i];
+    }
+}
+
+
+
+
+void show(int M, int N, int ** arr){
+    for (int i = 0; i < M; i++){
+        for (int j = 0; j < N; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << std::endl;
+    }
+}
+
+
+
+
+
+
+int main() {
+    int M, N;
+    cin >> M >> N;
+    int ** arr = (int**)malloc(M * sizeof(int**));
+    for (int i = 0; i < M; ++i) {
+        arr[i] = (int*)malloc(M * sizeof(int*));
+    }
+
+    for (int i = 0; i < M; ++i){
+        for (int j = 0; j < N; ++j){
+            int a;
+            cin >> a;
+            arr[i][j] = a;
+        }
+    }
+    show(M, N, arr);
+    for (int i = 0; i < N; i++) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
+
+
+
+
+
+    int S;
+    vector<int>vec;
+    cin >> S;
+    for (int i = 0; i < S; i++){
+        int a;
+        cin >> a;
+    }
+
+    show(vec, S);
+
+}
